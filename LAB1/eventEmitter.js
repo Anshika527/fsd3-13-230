0c8ae016a1df5522fb0a011ce28937d2160dda2c
Anshika <anshika.25b01010549@abes.ac.in>
2026-07-22 12:23:16 +0530

    Added eventEmitter.js

---
+++ LAB1/eventEmitter.js
@@ -0,0 +1,24 @@
+import { EventEmitter } from "node:events";
+const task = new EventEmitter();
+
+const sayHi = (name) => {
+  console.log(`Logged In ${name}`);
+};
+
+const starts = () => {
+  console.log("System Started");
+};
+
+task.once("greet", starts);
+task.on("greet", sayHi); //event method
+task.on("greet", (name) => {
+  console.log(`${name} starts shopping`);
+});
+
+task.on("greet", (name) => {
+  console.log(`${name} logged out`);
+});
+
+task.emit("greet", "Anshika"); //Announcement
+task.emit("greet", "Palak");
+task.emit("greet", "Mahi");
