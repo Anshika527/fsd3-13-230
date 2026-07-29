9813667a584af63f34f8fa854d540a68715ace52
Anshika <anshika.25b01010549@abes.ac.in>
2026-07-29 11:58:29 +0530

    FS usage

---
+++ LAB2/file1.js
@@ -0,0 +1,7 @@
+import { writeFile } from "fs/promises";
+
+/*await writeFile('stud.txt',"Name: Anshika Singh");
+ console.log("File Written");*/
+
+await writeFile("class.txt", "Name: Vimal sinha");
+console.log("File Written");
