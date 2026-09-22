95d959dc40b698cb802a85ecd40b58cf9d376f7c
Anshika <anshika.singh4126@gmail.com>
2026-09-22 14:17:40 +0530

    get all products completed

--- lab3/prg6.js
+++ lab3/prg6.js
@@ -1,10 +1,19 @@
 import http from "http";
+import { getAllProducts } from "./products.js";
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/" && req.method === "GET") {
+  if (req.url === "/api/v1/products" && req.method === "GET") {
     res.statusCode = 200;
-    res.end("GET Request");
-  } else if (req.url === "/" && req.method === "POST") {
+    const data = getAllProducts();
+    res.setHeader("content-type", "application/json");
+
+    res.end(
+      JSON.stringify({
+        count: data.length,
+        data,
+      }),
+    );
+  } else if (req.url === "/api/v1/products" && req.method === "POST") {
     //console.log("Request:", req);
     let body = "";
     req.on("data", (chunk) => {
