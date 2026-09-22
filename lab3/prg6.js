0303de2362f195b48a9859f2440058c0a16eeef9
Anshika <anshika.singh4126@gmail.com>
2026-09-22 14:56:31 +0530

    delete product updated uding api

--- lab3/prg6.js
+++ lab3/prg6.js
@@ -1,6 +1,7 @@
 import http from "http";
 import { getAllProducts } from "./products.js";
 import { addProducts } from "./products.js";
+import { deleteProduct } from "./products.js";
 
 const server = http.createServer((req, res) => {
   if (req.url === "/api/v1/products" && req.method === "GET") {
@@ -39,9 +40,18 @@ const server = http.createServer((req, res) => {
       res.statusCode = 200;
       res.end(JSON.stringify({ msg: "product UPDATED", product }));
     });
-  } else if (req.url === "/" && req.method === "DELETE") {
+  } else if (
+    req.url.startsWith("/api/v1/products/") &&
+    req.method === "DELETE"
+  ) {
+    const pid = Number(req.url.split("/").pop());
+
     res.statusCode = 200;
-    res.end("DELETE Request");
+    if (deleteProduct(pid)) {
+      res.end(JSON.stringify({ msg: "item deleted" }));
+    } else {
+      res.end(JSON.stringify({ msg: `products with id ${pid} not found` }));
+    }
   } else {
     res.statusCode = 404;
     res.end("request not found");
--- lab3/products.js
+++ lab3/products.js
@@ -15,3 +15,12 @@ export const addProducts = (item) => {
   products.push(item);
   return item;
 };
+
+export const deleteProduct=(pid)=>{
+    const item=products.findIndex((prd)=>prd.id==pid);
+    if (item==-1)
+        return false;
+    products.splice(item,1)
+    console.log("products remaining:",products);
+    return true;
+};
